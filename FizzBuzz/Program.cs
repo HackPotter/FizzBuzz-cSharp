5a6a0ae2e16222c6ed9608a196d23bfab616da33
Jack Potter <jpotter@iastate.edu>
2017-02-15 13:56:06 -0600

    Added project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Where should FizzBuzz start?");
+			int start = Convert.ToInt32 (Console.ReadLine ());
+			Console.WriteLine ("Where should FizzBuzz Stop?");
+			int stop = Convert.ToInt32 (Console.ReadLine ());
+			Console.WriteLine ($"FizzBuzz from {start} to {stop}:");
+			for (int i = start; i <= stop; ++i)
+			{
+				if (i % 3 == 0)
+				{
+					Console.Write ("Fizz");
+				}
+				if (i % 5 == 0)
+				{
+					Console.Write ("Buzz");
+				}
+				if (i % 3 != 0 && i % 5 != 0)
+				{
+					Console.Write(i);
+				}
+				Console.Write("\n");
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FizzBuzz-cSharp
+I wrote this while I was learning C# and I figured I'd throw it up on GitHub. It runs in the console, where it asks where to start and stop the FizzBuzz process. Fizz triggers on multiples of 3, Buzz on multiples of 5. 
